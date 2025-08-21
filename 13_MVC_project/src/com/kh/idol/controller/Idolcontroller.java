ec6e9bf2f04c3f7bea61c20403bc0566e0b7e44a
unknown <gustjd2754@daum.net>
2025-08-21 14:07:35 +0900

    aespa

--- 13_MVC_project/src/com/kh/idol/controller/Idolcontroller.java
+++ 13_MVC_project/src/com/kh/idol/controller/Idolcontroller.java
@@ -1,5 +1,23 @@
 package com.kh.idol.controller;
 
-public class Idolcontroller {
+import java.util.*;
 
+import com.kh.idol.model.vo.*;
+
+public class IdolController {
+
+	private List<Fan> fl = new ArrayList();
+	private List<Board> boards = new ArrayList();
+	private List<Idol> aespa = new ArrayList();
+	// 아이돌들도 어디에 담아놓고 고걸 읽어와서 정보를 출력해줘야 하지 않을까?
+	
+	
+	{
+		aespa.add(new Idol("카리나", "리더", "Rocket Puncher"));
+		aespa.add(new Idol("지젤", "외국인1", "Xenoglossy"));
+		aespa.add(new Idol("윈터", "불자", "Armamenter"));
+		aespa.add(new Idol("닝닝", "외국인2", "E.D Hacker"));
+		
+	}
+	
 }
--- 13_MVC_project/src/com/kh/idol/view/IdolView.java
+++ 13_MVC_project/src/com/kh/idol/view/IdolView.java
@@ -1,5 +1,30 @@
 package com.kh.idol.view;
 
+import java.util.*;
+
+import com.kh.idol.controller.*;
+
 public class IdolView {
 
+	// 필드부 
+	private Scanner sc = new Scanner(System.in);
+	private IdolController ic = new IdolController();
+	
+	// 생성자
+	
+	// 메소드부
+	// 프로그램 실행했을 떄 가장 먼저 보여질 화면을 출력해주는 기능
+	public void mainMenu() {
+		
+		System.out.println("\n\n ‼ aespa 커뮤니티에 오신것을 환영합니다 ‼ ");
+		
+		while(true) {
+			
+			
+			
+			
+			
+			
+		}
+	}
 }
