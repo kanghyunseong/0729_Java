3ca07debf12db2109594811eb99790354f935f86
unknown <gustjd2754@daum.net>
2025-08-21 16:17:38 +0900

    aespa

--- 13_MVC_project/src/com/kh/idol/controller/Idolcontroller.java
+++ 13_MVC_project/src/com/kh/idol/controller/Idolcontroller.java
@@ -116,53 +116,75 @@ public class IdolController {
 	}
 
 	public Idol findMember(int memberNo) {
-		
+
 		/*
-		if(memberNo == 1) {
-			return aespa.get(0);
-		}else if(memberNo == 1) {
-			
-		} else if(memberNo == 1) {
-			
-		}else {
-			
-		}
-		*/
-		return aespa.get(memberNo - 1); 
+		 * if(memberNo == 1) { return aespa.get(0); }else if(memberNo == 1) {
+		 * 
+		 * } else if(memberNo == 1) {
+		 * 
+		 * }else {
+		 * 
+		 * }
+		 */
+		return aespa.get(memberNo - 1);
 	}
-	
+
 	// 사용자가 아이디를 입력했을 떄 입력한 아이디가
 	// Fan들이 들어가는 List의 요소의 userId필드값과 중복되는게 있는지
 	// 확인을 한 뒤에 결과를 View로 다시 반환
 	public boolean checkId(String userId) {
-		
+
 		// 1. userId
 		// 2. fans
 		// 3. fans.get(i).getUserId()
 		// 4. equals()
-		
-		for(int i = 0; i < fans.size(); i ++) {
-			if(userId.equals(fans.get(i).getUesrId())) {
+
+		for (int i = 0; i < fans.size(); i++) {
+			if (userId.equals(fans.get(i).getUesrId())) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	// 사용자가 입력한 아이디, 비밀번호, 닉네임을 View로부터 전달받아
 	// 새로운 Fan이라는 객체를 생성하면서 필드에 값을 대입해주고
 	// 생성된 Fan개체의 주소값을 내가 가지고 있는 Fan들이 들어가있는 List에
 	// 추가를 해주는 메소드를 구현해야함
-	public void signUp(String userId, String userPwd, String nickName) {
-		// 1. 데이터 가공	
+	public boolean signUp(String userId, String userPwd, String nickName) {
+		// 1. 데이터 가공
 		Fan fan = new Fan(userId, userPwd, nickName);
-		
+
 		// 2. 요청 처리(아직 요청 처리하는 계층을 안배웠으므로 컨트롤러에서 처리)
-		fans.add(fan);
-		
-		
-		
-		
+		boolean result = checkId(userId);
+
+		if (!result) {
+			fans.add(fan);
+			return false;
+		}
+
+		// 3. 결과값 반환
+		return true;
 	}
 
+	// 사용자가 로그인 요청 시 호출되는 메소드
+	public Fan login(String userId, String userPwd) {
+
+		// 컨트롤러가 가지고 있는
+		// Fan의 정보를 필드에 저장하는 Fan객체들의 주소를 가지고 있는
+		// fans라는 리스트의 요소에 하나하나 접근해서
+		// Fan객체의 userId 및 userPwd필드를
+		// 사용자가 입력한 userId값 + userPwd값과 각각 비교하여
+		// 둘 다 일치하는 Fan객체가 존재한다면 사용자의 정보가 담겨있는 Fan을 반환
+
+		for (int i = 0; i < fans.size(); i++) {
+			// 반복하면서 순차적으로 접근할 요소 객체를 변수로 선언
+			Fan fan = fans.get(i);
+
+			if (userId.equals(fan.getUesrId()) && userPwd.equals(fans.getUserPwd())) {
+				return fan;
+			}
+		}
+		return null;
+	}
 }
--- 13_MVC_project/src/com/kh/idol/view/IdolView.java
+++ 13_MVC_project/src/com/kh/idol/view/IdolView.java
@@ -76,7 +76,7 @@ public class IdolView {
 		}
 
 	}
-	
+
 	private void findMember(int menuNo) {
 		Idol idol = ic.findMember(menuNo);
 		System.out.println("💝" + idol.getName() + "💝");
@@ -110,16 +110,16 @@ public class IdolView {
 		}
 
 	}
-	
+
 	public void signUp() {
 		System.out.println("\n에스파 커뮤니티에 오신것을 환영합니다.");
 		System.out.println("회원가입 서비스입니다.");
-		
+
 		String userId;
-		while(true) {
-			
+		while (true) {
+
 			// 제약조건 => 아이디는 4글자에서 10글자 사이일 것
-			//        => 아이디는 다른 Fan의 아이디와 중복되면 안될 것
+			// => 아이디는 다른 Fan의 아이디와 중복되면 안될 것
 			System.out.print("가입하실 아이디를 입력해주세요 > ");
 			userId = sc.nextLine();
 			// 하나의 메소드는 하나의 기능만을 수행해야한다.
@@ -130,32 +130,53 @@ public class IdolView {
 			// 2. 회원가입
 			// 두개가 되어버리기 떄문에 하는 일이 너무 많은 것이다.
 			// 1. 아이디는 4글자에서 10글자 사이만 가능
-			if(!(3 < userId.length() && userId.length() < 11)) {
+			if (!(3 < userId.length() && userId.length() < 11)) {
 				System.out.println("아이디는 4글자에서 10자 사이만 사용 가능합니다.");
 				continue;
 			}
-			
+
 			// 2. 중복체크
-			boolean duplicatedId= ic.checkId(userId);
-			
-			if(duplicatedId) {
+			boolean duplicatedId = ic.checkId(userId);
+
+			if (duplicatedId) {
 				System.out.println("이미 존재하는 아이디 입니다");
 			} else {
 				System.out.println("사용 가능한 아이디입니다.");
 				break;
 			}
 		}
-		
+
 		System.out.print("비밀번호를 입력해주세요 > ");
 		String userPwd = sc.nextLine();
-		
+
 		System.out.print("닉네임을 입력해주세요 > ");
 		String nickName = sc.nextLine();
-		
+
 		// 뷰는 일단 자신의 할일을 1절까지 마무리함
 		// 사용자가 입력한 아이디와 비밀번호와 닏네임을 가지고 Fan객체를 생성해서
 		// Controller에 있는 fans라는 List의 요소로 추가
-		ic.signUp(userId, userPwd, nickName);
+		boolean result = ic.signUp(userId, userPwd, nickName);
+		if (result) {
+			System.out.println("회원가입에 실패했씁니다 다시시도해주세요");
+		} else {
+			System.out.println("추카포카양파링~~");
+		}
+	}
+
+	private void login() {
+		System.out.println("\n 로그인 서비스 입니다.");
+
+		System.out.print("아이디를 입력하세요 > ");
+		String userId = sc.nextLine();
+
+		System.out.print("아이디를 입력하세요 > ");
+		String userPwd = sc.nextLine();
+
+		ic.login(userId, userPwd);
+		
+		
+		
+		
 	}
 
 }
